96641d5f4783a5a781893cc9ed432be1ae1f8811
Hamed Wael <hamed.wael.matter@gmail.com>
2022-10-05 22:47:05 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/keyam/beheira/hamed/keyam/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.keyam.beheira.hamed.keyam
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/keyam/dev/by/hamed/keyam/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.keyam.dev.by.hamed.keyam
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
